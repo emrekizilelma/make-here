592e529cd55360a7b2a9563411b6b261826cf312
emrekizilelma <emrekizilelma00@gmail.com>
2024-01-27 20:29:07 +0300

    file operations

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 #include <make.h>
 #include <getcwd.h>
@@ -14,7 +14,7 @@ int main(int argc, char* argv[]) {
         }
 
         else {
-            printf("No argument given\n");
+            printf("No argument given or wrong lang.\n");
         }
     }
   
--- src/make.c
+++ src/make.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
 void make_c() {
+    system("touch Makefile1");
 
+    FILE *in_file  = fopen("Makefile1", "r"); // read only
+          FILE *out_file = fopen("Makefile1", "w"); // write only
+          
+          // test for files not existing.
+          if (in_file == NULL || out_file == NULL)
+            {  
+              printf("Error! Could not open file\n");
+              exit(-1); // must include stdlib.h
+            }
+          
+          // write to file vs write to screen
+
+        char* data;
+
+          fprintf(out_file, "\n"); // write to file
 }
 
 void make_template(char* lang) {
     printf("\n[WORKING] make_template()\n");
+
+    if(!strcmp(lang, "C") || !strcmp(lang, "C++") || !strcmp(lang, "c") || !strcmp(lang, "cpp")) {
+            printf("Yes strcmp\n");
+        }
+
+    make_c();
     
 }
